21089e64d5fa07f8584a28c9bbaa45a1ed691206
MVozniak <mikola.v.2016@gmail.com>
2023-11-29 21:53:33 +0200

    Change WebAssembly to Server, Added Atentication

---
+++ Tumbleweed/Authentication/UserSession.cs
@@ -0,0 +1,9 @@
+﻿namespace Tumbleweed.Authentication
+{
+    public class UserSession
+    {
+        public string UserName { get; set; }
+
+        public string Role { get; set; }
+    }
+}
--- TumbleweedDb/dbo/Tables/Roles.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Roles] (
-    [Id]              INT            NOT NULL,
-    [ShopId]          INT            NOT NULL,
-    [RoleName]        NVARCHAR (50)  NOT NULL,
-    [RoleDescription] NVARCHAR (MAX) NULL,
-    CONSTRAINT [PK_Roles] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Roles_Shops] FOREIGN KEY ([ShopId]) REFERENCES [dbo].[Shops] ([Id])
-);
-
